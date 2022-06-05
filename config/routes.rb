80ac6a20dccaff579cd26a5608b8a4855739316d
mohammed rishad <mohammedrishad17@gmail.com>
2022-06-05 22:21:44 +0400

    minitest added

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root "site#index"
+  get '/site/index', to: 'site#index'
   get '/:row_id/:column_id', to: 'site#index', :constraints => { :row_id => /[0-9]+(\%7C[0-9]+)*/, :column_id => /[0-9]+(\%7C[0-9]+)*/ }
   resources :results, only: [:create, :show, :index] do
     collection do 
--- test/models/result_test.rb
+++ test/models/result_test.rb
@@ -1,7 +1,14 @@
 require "test_helper"
 
 class ResultTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'valid name' do
+    result = Result.new(name: 'Rishad', clicks: 7, time_taken: 100, game_board: "", high_score: 77.7)
+    assert result.valid?
+  end
+
+  test 'invalid without name' do
+    result = Result.new(clicks: 7, time_taken: 100, game_board: "", high_score: 77.7)
+    refute result.valid?, 'result is valid without a name'
+    assert_not_nil result.errors[:name], 'no validation error for name present'
+  end
 end
